b4dc8e44ec6f6606cb77f7ef741cbc1a539bac43
Aibol Bissenkul <aibol.biss@gmail.com>
2023-05-22 17:31:51 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// Prime Vue
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
---
+++ src/service/ProductService.js
@@ -0,0 +1,422 @@
+export const ProductService = {
+  getProductsData() {
+    return [
+      {
+        id: 1,
+        number: 1,
+        name_indicators: 'Установленная электрическая мощность',
+        unit: {
+          name_1: 'МВт'
+        },
+        y2017: {
+          number_1: '372,5'
+        },
+        y2018: {
+          number_1: '372,5'
+        },
+        y2019: {
+          number_1: '372,5'
+        },
+        y2020: {
+          number_1: '372,5'
+        },
+        y2021: {
+          number_1: '372,5'
+        }
+      },
+      {
+        id: 2,
+        number: 2,
+        name_indicators: 'Располагаемая электрическая мощность',
+        unit: {
+          name_1: 'МВт'
+        },
+        y2017: {
+          number_1: '268,8'
+        },
+        y2018: {
+          number_1: '327,8'
+        },
+        y2019: {
+          number_1: '320,7'
+        },
+        y2020: {
+          number_1: '320,8'
+        },
+        y2021: {
+          number_1: '320,9'
+        }
+      },
+      {
+        id: 3,
+        number: 3,
+        name_indicators: 'Установленная тепловая мощность',
+        unit: {
+          name_1: 'Гкал'
+        },
+        y2017: {
+          number_1: '881,7'
+        },
+        y2018: {
+          number_1: '868,9'
+        },
+        y2019: {
+          number_1: '859,9'
+        },
+        y2020: {
+          number_1: '859,9'
+        },
+        y2021: {
+          number_1: '859,9'
+        }
+      },
+      {
+        id: 4,
+        number: 4,
+        name_indicators: 'Располагаемая тепловая мощность',
+        unit: {
+          name_1: 'Гкал'
+        },
+        y2017: {
+          number_1: '881,7'
+        },
+        y2018: {
+          number_1: '868,9'
+        },
+        y2019: {
+          number_1: '859,9'
+        },
+        y2020: {
+          number_1: '859,9'
+        },
+        y2021: {
+          number_1: '859,9'
+        }
+      },
+      {
+        id: 5,
+        number: 5,
+        name_indicators: 'Выработка электрической энергии',
+        unit: {
+          name_1: 'млн.кВтч'
+        },
+        y2017: {
+          number_1: '1751,6'
+        },
+        y2018: {
+          number_1: '1801,6'
+        },
+        y2019: {
+          number_1: '2192,6'
+        },
+        y2020: {
+          number_1: '2247,0'
+        },
+        y2021: {
+          number_1: '2168,3'
+        }
+      },
+      {
+        id: 6,
+        number: 6,
+        name_indicators: 'Отпуск электрической энергии',
+        unit: {
+          name_1: 'млн.кВтч'
+        },
+        y2017: {
+          number_1: '1464,3'
+        },
+        y2018: {
+          number_1: '1499,0'
+        },
+        y2019: {
+          number_1: '1852,0'
+        },
+        y2020: {
+          number_1: '1901,3'
+        },
+        y2021: {
+          number_1: '1825,1'
+        }
+      },
+      {
+        id: 7,
+        number: 7,
+        name_indicators: 'Выработка тепловой энергии',
+        unit: {
+          name_1: 'тыс. Гкал'
+        },
+        y2017: {
+          number_1: '2560,0'
+        },
+        y2018: {
+          number_1: '2681,6'
+        },
+        y2019: {
+          number_1: '2603,8'
+        },
+        y2020: {
+          number_1: '2556,2'
+        },
+        y2021: {
+          number_1: '2630,0'
+        }
+      },
+      {
+        id: 8,
+        number: 8,
+        name_indicators: 'Отпуск тепловой энергии',
+        unit: {
+          name_1: 'тыс. Гкал'
+        },
+        y2017: {
+          number_1: '2449,2'
+        },
+        y2018: {
+          number_1: '2555,0'
+        },
+        y2019: {
+          number_1: '2479,2'
+        },
+        y2020: {
+          number_1: '2430,4'
+        },
+        y2021: {
+          number_1: '2503,5'
+        }
+      },
+      {
+        id: 9,
+        number: 9,
+        name_indicators: 'Собственные нужды электрическая энергия',
+        unit: {
+          name_1: 'млн. кВтч',
+          name_2: '%'
+        },
+        y2017: {
+          number_1: '272,1',
+          number_2: '15,5'
+        },
+        y2018: {
+          number_1: '285,3',
+          number_2: '15,8'
+        },
+        y2019: {
+          number_1: '323,4',
+          number_2: '14,7'
+        },
+        y2020: {
+          number_1: '326,7',
+          number_2: '14,5'
+        },
+        y2021: {
+          number_1: '324,8',
+          number_2: '15,0'
+        }
+      },
+      {
+        id: 10,
+        number: 10,
+        name_indicators: 'Собственные нужды тепловая энергия',
+        unit: {
+          name_1: 'тыс. Гкал',
+          name_2: '%'
+        },
+        y2017: {
+          number_1: '106,1',
+          number_2: '4,1'
+        },
+        y2018: {
+          number_1: '121,8',
+          number_2: '4,5'
+        },
+        y2019: {
+          number_1: '119,9',
+          number_2: '4,6'
+        },
+        y2020: {
+          number_1: '121,0',
+          number_2: '4,7'
+        },
+        y2021: {
+          number_1: '121,7',
+          number_2: '4,6'
+        }
+      },
+      {
+        id: 11,
+        number: 11,
+        name_indicators: 'Коэффициент использования установленной электрической мощности',
+        unit: {
+          name_1: '%'
+        },
+        y2017: {
+          number_1: '53,7'
+        },
+        y2018: {
+          number_1: '55,2'
+        },
+        y2019: {
+          number_1: '67,2'
+        },
+        y2020: {
+          number_1: '68,7'
+        },
+        y2021: {
+          number_1: '66,4'
+        }
+      },
+      {
+        id: 12,
+        number: 12,
+        name_indicators: 'Коэффициент использования установленной теплововй мощности',
+        unit: {
+          name_1: '%'
+        },
+        y2017: {
+          number_1: '33,1'
+        },
+        y2018: {
+          number_1: '35,2'
+        },
+        y2019: {
+          number_1: '34,6'
+        },
+        y2020: {
+          number_1: '33,8'
+        },
+        y2021: {
+          number_1: '34,9'
+        }
+      },
+      {
+        id: 13,
+        number: 13,
+        name_indicators: 'Удельный расход топлива на электрическую энергию',
+        unit: {
+          name_1: 'гут/кВтч',
+          name_2: 'г.н.т./кВтч'
+        },
+        y2017: {
+          number_1: '290,1',
+          number_2: '-'
+        },
+        y2018: {
+          number_1: '299,6',
+          number_2: '-'
+        },
+        y2019: {
+          number_1: '313,2',
+          number_2: '-'
+        },
+        y2020: {
+          number_1: '320,1',
+          number_2: '-'
+        },
+        y2021: {
+          number_1: '312,6',
+          number_2: '-'
+        }
+      },
+      {
+        id: 14,
+        number: 14,
+        name_indicators: 'Удельный расход топлива на тепловую энергию',
+        unit: {
+          name_1: 'кгут/Гкал',
+          name_2: 'кг.н.т./Гкал'
+        },
+        y2017: {
+          number_1: '185,1',
+          number_2: '-'
+        },
+        y2018: {
+          number_1: '184,5',
+          number_2: '-'
+        },
+        y2019: {
+          number_1: '184,0',
+          number_2: '-'
+        },
+        y2020: {
+          number_1: '184,4',
+          number_2: '-'
+        },
+        y2021: {
+          number_1: '181,7',
+          number_2: '-'
+        }
+      },
+      {
+        id: 15,
+        number: 15,
+        name_indicators: 'Общий КПД электростанции',
+        unit: {
+          name_1: '%'
+        },
+        y2017: {
+          number_1: '60,2'
+        },
+        y2018: {
+          number_1: '59,6'
+        },
+        y2019: {
+          number_1: '56,0'
+        },
+        y2020: {
+          number_1: '54,9'
+        },
+        y2021: {
+          number_1: '56,7'
+        }
+      },
+      {
+        id: 16,
+        number: 16,
+        name_indicators: 'Износ электростанции*',
+        unit: {
+          name_1: '%'
+        },
+        y2017: {
+          number_1: '74,1'
+        },
+        y2018: {
+          number_1: '58,1'
+        },
+        y2019: {
+          number_1: '60,6'
+        },
+        y2020: {
+          number_1: '63,0'
+        },
+        y2021: {
+          number_1: '64,9'
+        }
+      }
+    ]
+  },
+
+  getProductsMini() {
+    return Promise.resolve(this.getProductsData().slice(0, 5))
+  },
+
+  getProductsSmall() {
+    return Promise.resolve(this.getProductsData().slice(0, 10))
+  },
+
+  getProductsBig() {
+    return Promise.resolve(this.getProductsData().slice(0, 100))
+  },
+
+  getProducts() {
+    return Promise.resolve(this.getProductsData())
+  },
+
+  getProductsWithOrdersSmall() {
+    return Promise.resolve(this.getProductsWithOrdersData().slice(0, 10))
+  },
+
+  getProductsWithOrders() {
+    return Promise.resolve(this.getProductsWithOrdersData())
+  }
+}
